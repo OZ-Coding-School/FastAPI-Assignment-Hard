61cd31f7296fdc8a63eb474f13ad13bb63430adc
meoyoug <eodyd426@gmail.com>
2025-03-14 13:53:19 +0900

    - modify user_model_clear()   - add MovieModel.clear()

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+from src.models.movies import MovieModel
 from src.models.users import UserModel
 
 TEST_BASE_URL = "http://test"
@@ -8,3 +9,4 @@ TEST_BASE_URL = "http://test"
 @pytest.fixture(scope="function", autouse=True)
 def user_model_clear() -> None:
     UserModel.clear()
+    MovieModel.clear()
