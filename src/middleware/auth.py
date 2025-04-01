b4513be21d9edaaa3fcbe17fb1d040ddf051c804
meoyoug <eodyd426@gmail.com>
2025-04-01 16:04:17 +0900

    - src/middleware/auth.py   - add NEED_AUTH_REGEX_URL   - refactor AuthMiddleware

--- src/middleware/auth.py
+++ src/middleware/auth.py
@@ -1,3 +1,5 @@
+import re
+
 from fastapi import HTTPException
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request
@@ -5,13 +7,25 @@ from starlette.responses import JSONResponse, Response
 
 from src.services.auth import AuthService
 
+NEED_AUTH_REGEX_URL = [
+    r"^/users/me$",
+    r"^/users/me/profile_image$",
+    r"^/users/me/reviews$",
+    r"^/reviews$",
+    r"^/reviews/\d+$",
+    r"^/reviews/\d+/is_liked$",
+    r"^/likes/reviews/\d+/like$",
+    r"^/likes/reviews/\d+/unlike$",
+]
+
 
 class AuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
         try:
-            if request.url.path.startswith("/users"):
-                if request.url.path not in ["/users/login", "/users", "/users/search"]:
+            for url in NEED_AUTH_REGEX_URL:
+                if re.match(url, request.url.path):
                     request = await AuthService().get_current_user(request)
+                    break
             response: Response = await call_next(request)
             return response
         except HTTPException as e:
